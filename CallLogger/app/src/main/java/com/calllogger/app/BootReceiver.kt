76fd5544d4bb3504553430d48609434667d1e376
TheBigThree <toxicplyayzopp@gmail.com>
2025-10-04 21:38:20 +0530

    Add files via upload

---
+++ CallLogger/app/src/main/java/com/calllogger/app/BootReceiver.kt
@@ -0,0 +1,16 @@
+﻿package com.calllogger.app
+import android.content.*
+import android.os.Build
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+            val serviceIntent = Intent(context, CallLoggerService::class.java)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                context.startForegroundService(serviceIntent)
+            } else {
+                context.startService(serviceIntent)
+            }
+        }
+    }
+}
